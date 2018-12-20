214aca0130a792d48474b8e3dc719f15a803a5d6
Jack Branch <jackbranch@ECS0821.local>
2018-12-20 12:11:28 +0000

    1st commit

---
+++ src/main/java/com/stocktrainer/stockJb/StockJbApplication.java
@@ -0,0 +1,14 @@
+package com.stocktrainer.stockJb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockJbApplication.class, args);
+	}
+
+}
+
